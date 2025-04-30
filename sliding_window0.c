e7663c435d2bbca95ad95551af7fbda87a221ccf
Vishal Kompalli <vishal.kompalli@gmail.com>
2025-04-30 16:02:06 +0530

    Added comments and improved code readabiity.

--- sliding_window0.c
+++ sliding_window0.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
-#include <limits.h>
+#include <limits.h> // For INT_MIN
 
+// Function to find the maximum sum of a subarray of size k
 int maxSubarraysum(int arr[], int n, int k)
 {
-    // int current_sum = 0;
-    // int max_sum=0;
     if(n < k)
     {
         printf("Invalid");
-        return INT_MIN;
+        return INT_MIN;// Indicate an error
     }
+
+    // Initialize the maximum sum and the current window sum
     int max_sum = INT_MIN;
     int current_sum = 0;
 
+    // Calculate the sum of the first window of size k
     for(int i=0;i<k;i++)
     {
         current_sum+=arr[i];
     }
+
+    // Initialize max_sum with the sum of the first window
     max_sum = current_sum;
 
+    // Slide the window one element at a time
     for(int i=k;i<n;i++)
     {
-        current_sum-=arr[i-k];
-        current_sum+=arr[k];
+        current_sum-=arr[i-k]; // Subtract the first element of the previous window
+        current_sum+=arr[k]; // Add the last element of the current window
+
+        // Update max_sum if the current window sum is greater
         if(current_sum > max_sum)
         {
             max_sum = current_sum;
